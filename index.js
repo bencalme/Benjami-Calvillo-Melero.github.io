cc8802ab2ff7c21a4077a4923094ff4d5760bf18
Benjamí Calvillo Melero <142400977+bencalme@users.noreply.github.com>
2024-06-04 21:00:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("El sitio web de Benjam’ Calvillo Melero est‡ listo.");
+    console.log("El sitio web de Benjamí Calvillo Melero está listo.");
+    
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
